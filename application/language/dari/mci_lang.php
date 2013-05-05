79fe4ea40dc25611b7a47284d3163ed56ad76ae7
unknown <rafiq@.(none)>
2013-05-05 08:27:35 +0430

    v

--- application/language/dari/mci_lang.php
+++ application/language/dari/mci_lang.php
@@ -84,10 +84,10 @@ $lang['lan'] = "da";
 $lang['land'] = "pa";
 
 //end head menus.....................................
-$lang['mci_example_1'] = "Привет, я <strong>первый</strong> содержание этого документа HTML5!";
-$lang['mci_example_2'] = "Привет, я <strong>второй</strong> Содержание этого документа HTML5!";
-$lang['mci_click'] = "Нажмите здесь!";
-$lang['mci_footer'] = "Привет, я подвал!";
+$lang['mci_example_1'] = "asdfasdfasdfasdfasdfasdfasdfasdfasdf";
+$lang['mci_example_2'] = "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf";
+$lang['mci_click'] = "asdfasdfasdfasdf";
+$lang['mci_footer'] = "asdfasdfadsfasdfasdfasdfasdfsadf";
 $lang['new1'] = "newsesd";
 
 /* End of file mci_lang.php */
